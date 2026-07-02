19e172fe5c46e62b4cbadefa766cfe4330353623
behy269 <lengocphuongdien260909@gmail.com>
2026-07-02 19:22:24 +0700

    Add Discord bot initialization in index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Đăng nhập thành công: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
